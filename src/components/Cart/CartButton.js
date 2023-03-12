06d42beedf762954d008c9128f43d55d12cbe43f
HakantktDev <hakantokatdev@gmail.com>
2023-03-12 21:22:23 +0300

    redux-toolkig-and-ui-state-added

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toogleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toogleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,13 +1,7 @@
-import classes from './Card.module.css';
+import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return (
-    <section
-      className={`${classes.card} ${props.className ? props.className : ''}`}
-    >
-      {props.children}
-    </section>
-  );
+  return <section className={`${classes.card} ${props.className ? props.className : ""}`}>{props.children}</section>;
 };
 
 export default Card;
