5f453c3e35ba8a256c613431e5dc9e4db9f38df1
HakantktDev <hakantokatdev@gmail.com>
2023-03-12 22:54:31 +0300

    using-use-effect-with-redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-1c3b2-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
